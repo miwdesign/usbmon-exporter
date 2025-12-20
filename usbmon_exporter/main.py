96581590f681c459f66d8d09af2cc8e57c499e62
Simon Holesch <simon@holesch.de>
2025-12-20 13:15:45 +0100

    Receive and print uevents
    uevents are needed to map usbmon events to a specific USB port
    (devpath). For now just receive and print them.

--- usbmon_exporter/main.py
+++ usbmon_exporter/main.py
@@ -3,11 +3,29 @@
 import asyncio
 import contextlib
 
+from . import _uevent as uevent
 from . import _usbmon as usbmon
 
 
 async def main():
-    with usbmon.UsbMon("/dev/usbmon0"):
+    with (
+        usbmon.UsbMon("/dev/usbmon0"),
+        uevent.UEvent() as uev,
+    ):
+        loop = asyncio.get_running_loop()
+
+        def uevent_callback():
+            for event in uev.receive_iter():
+                if (
+                    event["ACTION"] in ("add", "remove")
+                    and event.get("DEVTYPE") == "usb_device"
+                ):
+                    print(
+                        f'{event["ACTION"]} Devnum: {event["DEVNUM"]}, Busnum: {event["BUSNUM"]} = {event["DEVPATH"]}'
+                    )
+
+        loop.add_reader(uev.sock.fileno(), uevent_callback)
+
         await asyncio.Event().wait()
 
 
