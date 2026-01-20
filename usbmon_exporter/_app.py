cb6e13bb466cf13cc5ab55df546010574d2a7bdd
Simon Holesch <simon@holesch.de>
2026-01-20 12:26:26 +0100

    Add command line interface
    Make port and usbmon device path configurable.
    
    Choose the next free port in the list of prometheus exporters as a
    default port:
    https://github.com/prometheus/prometheus/wiki/Default-port-allocations

--- usbmon_exporter/_app.py
+++ usbmon_exporter/_app.py
@@ -1,3 +1,4 @@
+import argparse
 import contextlib
 
 import prometheus_client as prometheus
@@ -8,11 +9,27 @@ from ._usbmon import UsbMon
 
 
 def main():
+    parser = argparse.ArgumentParser(
+        description="usbmon Prometheus Exporter",
+    )
+    parser.add_argument(
+        "-l",
+        "--listen-address",
+        default="0.0.0.0:10040",
+        help="Address to listen on for metrics exposition",
+    )
+    parser.add_argument(
+        "-d", "--device", default="/dev/usbmon0", help="Path to the usbmon device"
+    )
+    args = parser.parse_args()
+
+    host, port = args.listen_address.split(":")
+
     with (
-        UsbMon("/dev/usbmon0") as usbmon,
+        UsbMon(args.device) as usbmon,
         UEvent() as uevent,
         Exporter(usbmon, uevent) as exporter,
     ):
-        prometheus.start_http_server(8000)
+        prometheus.start_http_server(int(port), host)
         with contextlib.suppress(KeyboardInterrupt):
             exporter.run_forever()
